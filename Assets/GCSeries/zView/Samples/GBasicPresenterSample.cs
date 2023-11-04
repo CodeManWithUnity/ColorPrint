61e1cdc6e073ec6ce2795970ab34c87092cc1386
Giteetao <2985826650@qq.com>
2023-11-04 20:14:03 +0800

    1

---
+++ Assets/GCSeries/zView/Samples/GBasicPresenterSample.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace GCSeries.zView.Samples
+{
+    public class GBasicPresenterSample : MonoBehaviour
+    {
+        private GView _gView = null;
+        private GView gView
+        {
+            get
+            {
+                if (_gView == null)
+                    _gView = FindObjectOfType<GView>();
+                return _gView;
+            }
+        }
+
+        [SerializeField]
+        Button noModeButton = null;
+
+        [SerializeField]
+        Button TwoDModeButton = null;
+
+        [SerializeField]
+        Button ThreeDModeButton = null;
+
+        [SerializeField]
+        Button ARModeButton = null;
+        //////////////////////////////////////////////////////////////////
+        // Unity Monobehaviour Callbacks
+        //////////////////////////////////////////////////////////////////
+
+        void Start()
+        {
+            // Perform any additional setup for the sample scene.
+            this.SetUpScene();
+
+            if (gView == null)
+            {
+                Debug.LogError("场景内缺少GView组件");
+                return;
+            }
+
+            // 启动2D投屏
+            this.SetConnection(gView.GetStandardMode());
+
+            // 绑定UI事件
+            if (noModeButton != null)
+            {
+                noModeButton.onClick.AddListener(() =>
+                {
+                    this.SetConnection(IntPtr.Zero);
+                });
+            }
+
+            if (TwoDModeButton != null)
+            {
+                TwoDModeButton.onClick.AddListener(() =>
+                {
+                    this.SetConnection(gView.GetStandardMode());
+                });
+            }
+
+            if (ThreeDModeButton != null)
+            {
+                ThreeDModeButton.onClick.AddListener(() =>
+                {
+                    this.SetConnection(gView.GetThreeDMode());
+                });
+            }
+
+            if (ARModeButton != null)
+            {
+                ARModeButton.onClick.AddListener(() =>
+                {
+                    this.SetConnection(gView.GetAugmentedRealityMode());
+                });
+            }
+        }
+
+        void OnApplicationQuit()
+        {
+            // 在程序退出的时候如果结束录屏,该函数可以重复调用.
+            if (gView != null)
+                gView.FinishVideoRecording(IntPtr.Zero);
+        }
+
+        //////////////////////////////////////////////////////////////////
+        // Private Methods
+        //////////////////////////////////////////////////////////////////
+
+        private void SetConnection(IntPtr mode)
+        {
+            // 如果没创建连接，先创建一个
+            if (gView.GetCurrentActiveConnection() == IntPtr.Zero)
+                gView.ConnectToDefaultViewer();
+
+            if (mode == IntPtr.Zero)
+            {
+                // 设置为不投屏模式,直接关闭投屏
+                gView.SetConnectionMode(gView.GetCurrentActiveConnection(), IntPtr.Zero);
+                return;
+            }
+
+            // 先检查投屏模式的可用性
+            if (this.IsModeAvailable(gView.GetCurrentActiveConnection(), mode))
+            {
+                // 可用性检查通过,启动投屏
+                gView.SetConnectionMode(gView.GetCurrentActiveConnection(), mode);
+            }
+            else
+            {
+                Debug.LogError($"当前设备环境不支持 {mode} 投屏模式");
+            }
+        }
+
+        private bool IsModeAvailable(IntPtr connection, IntPtr mode)
+        {
+            if (connection != IntPtr.Zero)
+            {
+                int numSupportedModes = gView.GetNumConnectionSupportedModes(connection);
+                for (int i = 0; i < numSupportedModes; ++i)
+                {
+                    GView.SupportedMode supportedMode = gView.GetConnectionSupportedMode(connection, i);
+                    if (supportedMode.Mode == mode)
+                    {
+                        return (supportedMode.ModeAvailability == GView.ModeAvailability.Available);
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        private void SetUpScene()
+        {
+            // Cache references to objects in the scene.
+            _cubeObject = GameObject.Find("Cube");
+            _sphereObject = GameObject.Find("Sphere");
+            _capsuleObject = GameObject.Find("Capsule");
+            _leftPillarObject = GameObject.Find("LeftPillar");
+            _rightPillarObject = GameObject.Find("RightPillar");
+
+            // Update each scene object's color.
+            this.SetGameObjectColor(_cubeObject, Color.white);
+            this.SetGameObjectColor(_sphereObject, Color.red);
+            this.SetGameObjectColor(_capsuleObject, Color.green);
+            this.SetGameObjectColor(_leftPillarObject, Color.yellow);
+            this.SetGameObjectColor(_rightPillarObject, Color.grey);
+        }
+
+        private void Update()
+        {
+            // Spin the cube to more easily see the effects of pausing an active 
+            // connection's mode.
+            if (_cubeObject != null)
+            {
+                // Rotate the cube 30 degrees per second about its local y-axis.
+                Vector3 currentRotation = _cubeObject.transform.localRotation.eulerAngles;
+                currentRotation.y += (30.0f * Time.deltaTime);
+
+                _cubeObject.transform.localRotation = Quaternion.Euler(currentRotation);
+            }
+        }
+
+        private void SetGameObjectColor(GameObject gameObject, Color color)
+        {
+            if (gameObject == null)
+            {
+                return;
+            }
+
+            Renderer renderer = gameObject.GetComponent<Renderer>();
+            if (renderer == null)
+            {
+                return;
+            }
+
+            renderer.material = new Material(Shader.Find("Diffuse"));
+            renderer.material.color = color;
+        }
+
+
+        //////////////////////////////////////////////////////////////////
+        // Private Members
+        //////////////////////////////////////////////////////////////////
+        private GameObject _cubeObject = null;
+        private GameObject _sphereObject = null;
+        private GameObject _capsuleObject = null;
+        private GameObject _leftPillarObject = null;
+        private GameObject _rightPillarObject = null;
+    }
+}
+
---
+++ Assets/Scripts/PanelScripts/StartPanel.cs
@@ -0,0 +1,81 @@
+using Microsoft.Win32;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartPanel : BasePanel
+{
+    private static string name = "StartPanel";
+    private static string path = "Panel/StartPanel";
+    public static readonly UIType uIType = new UIType(path, name);
+    //看色与展色按钮
+    private Button ColorShowBtn;
+    //涂料库管理按钮
+    private Button ColorFormulaBtn;
+    //材料选择按钮
+    private Button ColorMaterialSelBtn;
+    //调色仿色训练
+    private Button ColorCombinationBtn;
+    //关闭按钮
+    private Button CloseBtn;
+    public StartPanel() : base(uIType)
+    {
+
+    }
+    public override void OnDestory()
+    {
+        base.OnDestory();
+    }
+    public override void OnDisable()
+    {
+        Debug.Log("StartPanel is back");
+        base.OnDisable();
+    }
+    public override void OnEnable()
+    {
+        base.OnEnable();
+    }
+    public override void OnStart()
+    {
+        base.OnStart();
+        ColorShowBtn = UIMethods.GetInstance().GetOrAddSingleComponentInChild<Button>(ActiveObj, "ColorShowBtn");
+        ColorShowBtn.onClick.AddListener(OnColorShowClick);
+        ColorFormulaBtn = UIMethods.GetInstance().GetOrAddSingleComponentInChild<Button>(ActiveObj, "ColorFormulaBtn");
+        ColorFormulaBtn.onClick.AddListener(OnColorFormulaClick);
+        ColorMaterialSelBtn = UIMethods.GetInstance().GetOrAddSingleComponentInChild<Button>(ActiveObj, "ColorMaterialSelBtn");
+        ColorMaterialSelBtn.onClick.AddListener(OnColorMaterialSelClick);
+        ColorCombinationBtn = UIMethods.GetInstance().GetOrAddSingleComponentInChild<Button>(ActiveObj, "ColorCombinationBtn");
+        ColorCombinationBtn.onClick.AddListener(OnColorCombinationClick);
+        CloseBtn = UIMethods.GetInstance().GetOrAddSingleComponentInChild<Button>(ActiveObj, "CloseBtn");
+        CloseBtn.onClick.AddListener(OnCloseClick);
+
+    }       
+    public void OnColorShowClick() 
+    {
+        Debug.Log("展色按钮点击");
+        GameRoot.GetInstance().UIManager_Root.PopAll();
+        ShowColorPanel showColorPanel = new ShowColorPanel();
+        GameRoot.GetInstance().UIManager_Root.Push(showColorPanel);
+    }
+    public void OnColorFormulaClick() 
+    {
+        Debug.Log("涂料库管理按钮点击");
+    }
+    public void OnColorMaterialSelClick()
+    {
+        Debug.Log("材料选择按钮点击");
+    }
+    public void OnColorCombinationClick() 
+    {
+        Debug.Log("调色仿色训练按钮点击");
+    }
+    public void OnCloseClick() 
+    {
+#if UNITY_EDITOR
+        Debug.Log("关闭");
+#endif
+        Application.Quit();
+    }
+}
